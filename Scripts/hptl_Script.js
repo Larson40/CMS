300d6751938b0c0c0e768bb82bca5dce3956bd45
Cooper Larson <supercoop40.cl@gmail.com>
2021-06-01 13:46:45 -0500

    Removed Unused Images

---
+++ Scripts/hptl_Script.js
@@ -0,0 +1,32 @@
+
+function Open_hp() {
+  location.href = "Hall_Pass";
+};
+
+function Open_tl() {
+  location.href = "Tardy_Logs";
+};
+
+function Close_form() {
+  location.href = "/HP_TL/RTS.php"
+};
+
+function upd_pass() {
+  location.href = "upd_pass.php";
+};
+
+function upd_TL() {
+  location.href = "upd_TL.php";
+};
+
+function ret_hm() {
+  location.href = "/HP_TL"
+}
+
+function try_again() {
+  location.href = "/HP_TL/Hall_Pass"
+}
+
+function try_againT() {
+  location.href = "/HP_TL/Tardy_Logs"
+}
